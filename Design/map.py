c4505b63928f44f29078888cac0f8ff327eb1b06
eddie94 <myeddie@naver.com>
2017-11-22 18:50:31 +0900

    월드맵 2 이동로직 완료 디자인 넣어야됨

--- Design/map.py
+++ Design/map.py
@@ -19,6 +19,9 @@ map_id=[0]
 map_id has the number of each rooms
 my room = 0
 world map = 1
+conv_store = 2
+world map 2 = 3
+nogada = 4
 '''
 
 def stage1(char_list):              #remember char_list[0] is image_list
@@ -30,6 +33,7 @@ def stage1(char_list):              #remember char_list[0] is image_list
         #print(image)
         quit_op()
         move_char(room_floor_size,char_list[1])
+        info(mychar)
 
         stage_end = stage1_design(mychar)
         screen.blit(char_list[0][mychar.image_index], mychar.rect)
@@ -78,24 +82,72 @@ def world_map(char_list):
     red_carpet_design = red_carpet.set_design("D:\학업자료\pycharm\hell_josun\images\map\Red_carpet.gif")
     red_carpet_size = red_carpet.set_size(550,670,100,50)
 
-    object_list = [conv_store]
+    object_list = [nogada,conv_store]
 
     while not stage_end:
         quit_op()
         # if not mychar.rect.colliderect(conv_store.rect):
         #     move_char(world_map_floor_size,char_list[1])
         mychar.move(object_list,world_map_floor_size)
-        print(mychar.rect.top, conv_store.rect.bottom)
+        print(mychar.rect)
+        #print(mychar.rect.left in range(nogada.rect.left, nogada.rect.right) or mychar.rect.right in range(nogada.rect.left, nogada.rect.right))
+        #print(conv_store.rect, nogada.rect)
         screen.blit(world_map_floor_design,(0,0))
         screen.blit(red_carpet_design, red_carpet.rect)
         screen.blit(conv_store_design, (0,0))
         screen.blit(conv_store_carpet_design,(315,312))
+        screen.blit(nogada_design, nogada.rect)
         screen.blit(char_list[0][mychar.image_index], mychar.rect)
         pygame.display.flip()
 
         if mychar.rect.bottom == red_carpet.rect.bottom and mychar.rect.right < red_carpet.rect.right and mychar.rect.left > red_carpet.rect.left:
             mychar.locus = [1,0,0]
             stage_end = True
+        if mychar.rect.top == 312 and mychar.rect.left in range(316,392):
+            mychar.locus = [1,2,0]
+            stage_end = True
+        if mychar.rect.right == 1280:
+            mychar.locus = [1,3,0]
+            stage_end = True
+
+
+def store(char_list):
+    '''
+    편의점 map_id : 2
+    :return:
+    '''
+    mychar = char_list[1]
+    stage_end = False
+
+    store_floor = object()
+    store_floor_design = store_floor.set_design("D:\학업자료\pycharm\hell_josun\images\map\conv_floor.jpg")
+    store_floor_size = store_floor.set_size(340,60,600,600)
+
+    fake_object = object()              #move를 위한 더미 오브젝트 추후 수정 필요
+    fake_object.set_size(0,0,0,0)
+
+    while not stage_end:
+        object_list = [fake_object]
+        quit_op()
+
+        mychar.move(object_list,store_floor_size)
+
+        if mychar.rect.bottom == 660 and mychar.rect.left in range(340,408):
+            mychar.locus = [2,1,0]
+            stage_end = True
+
+        screen.fill(black)
+        screen.blit(store_floor_design,store_floor.rect)
+        screen.blit(conv_store_carpet_design, (340,610))
+        screen.blit(char_list[0][mychar.image_index],mychar.rect)
+        pygame.display.flip()
+
+def world_map2():
+    '''
+    world_map map_id = 3
+    :return:
+    '''
+    pass
 
 
 def move_rooms(char_list):
@@ -117,3 +169,14 @@ def move_rooms(char_list):
         char_list[1].rect.top = 200
         char_list[1].rect.left = 775
         stage1(char_list)
+    elif previous_map_id == 1 and next_map_id == 2:
+        char_list[1].rect.bottom = 640
+        char_list[1].rect.left = 370
+        store(char_list)
+    elif previous_map_id == 2 and next_map_id == 1:
+        char_list[1].rect.top = 320
+        char_list[1].rect.left = 355
+        world_map(char_list)
+    elif previous_map_id == 1 and next_map_id == 3:
+        char_list[1].rect.left = 0
+        world_map(char_list)
